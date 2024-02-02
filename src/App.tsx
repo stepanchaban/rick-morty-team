98839d539d692dde583d3f080bb4903536fa92b6
Stepan <xxlnamne@gmail.com>
2024-02-02 10:02:11 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import './App.css';
 
 localStorage.setItem('string', 'privet');
-console.log(localStorage.getItem('string'));
 
 export const App: React.FC = () => <div>ASD</div>;
