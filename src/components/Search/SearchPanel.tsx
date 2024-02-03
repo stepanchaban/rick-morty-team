107455b3f3616cf84ffb2af8f53bff597fe47c4e
Alina Laniuk <alinavoronina16@gmail.com>
2024-02-03 17:07:28 +0200

    refactor: onChange function

--- src/components/Search/SearchPanel.tsx
+++ src/components/Search/SearchPanel.tsx
@@ -1,20 +1,24 @@
 import { ReactElement, useRef } from "react";
-import {debounce} from "../../utils/debounce";
+import { debounce } from "../../utils/debounce";
 
 const delay = 2000;
 
 function SearchPanel(): ReactElement {
     const ref = useRef<HTMLInputElement | null>(null);
 
-    const onChange = (): void => {
+    function logCurrentInputValue(): void {
         console.log(ref.current?.value);
     }
 
-    const debouncedOnChange = debounce(onChange, delay);
+    const debouncedOnChange = debounce(logCurrentInputValue, delay);
+
+    function onChange(): void{
+        debouncedOnChange();
+    }
 
     return (
         <div>
-            <input onChange={(): void => debouncedOnChange()} ref={ref} type="text" />
+            <input onChange={onChange} ref={ref} type="text" />
         </div>
     )
 };
