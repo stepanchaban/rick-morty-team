b5d4e4061eab976ac1301be8f7f9e286b9cc403c
Alina Laniuk <alinavoronina16@gmail.com>
2024-02-10 12:18:07 +0200

    feat: add comon type for card

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,44 +1,45 @@
-import { ReactElement } from "react";
-import Card from "@components/CardList/Card/Card";
-import styled from "styled-components";
-import { useGetCharactersQuery } from "@services/rickMorthyApi";
+import { ReactElement } from 'react';
+import Card from '@components/CardList/Card/Card';
+import styled from 'styled-components';
+import { useGetCharactersQuery } from '@services/rickMorthyApi';
 
 const CardListWrap = styled.div`
-    padding: 0 50px;
-`
+  padding: 0 50px;
+`;
 const CardListContent = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 30px;
-`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 30px;
+`;
 
 function CardList(): ReactElement {
-    const { data, error, isLoading } = useGetCharactersQuery();
+  const { data, error, isLoading } = useGetCharactersQuery();
 
-    const content = isLoading ? <div>Loading...</div> : 
-    data?.results.map((item, index) => {
-        return (
-            <div key={index}>
-                <Card
-                    src = {item.image}
-                    name = {item.name}
-                    species = {item.species}
-                    gender = {item.gender}
-                    status = {item.status}
-                />
-            </div>
-        )
+  const content = isLoading ? (
+    <div>Loading...</div>
+  ) : (
+    data?.map((item, index) => {
+      return (
+        <div key={index}>
+          <Card
+            src={item.src}
+            name={item.name}
+            species={item.species}
+            gender={item.gender}
+            status={item.status}
+          />
+        </div>
+      );
     })
+  );
 
-    return (
-        <CardListWrap>
-            <CardListContent>
-                {content}
-            </CardListContent>
-        </CardListWrap>
-    )
+  return (
+    <CardListWrap>
+      <CardListContent>{content}</CardListContent>
+    </CardListWrap>
+  );
 }
 
 export default CardList;
--- src/services/rickMorthyApi.ts
+++ src/services/rickMorthyApi.ts
@@ -1,12 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Character, AllCharactersResponse } from './types';
+import { Card } from 'types/Card';
 
 export const rickMorthyApi = createApi({
   reducerPath: 'rickMorthyApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api' }),
   endpoints: endpointsBuilder => ({
-    getCharacters: endpointsBuilder.query<AllCharactersResponse, void>({
+    getCharacters: endpointsBuilder.query<Card[], void>({
       query: () => `character`,
+      transformResponse: (response: AllCharactersResponse) => {
+        return response.results.map((item) => {
+          return {
+            src: item.image,
+            name: item.name,
+            species: item.species,
+            gender: item.gender,
+            status: item.status,
+          }
+        })
+      }
     }),
     getFilteredCharacters: endpointsBuilder.query<
       AllCharactersResponse,
